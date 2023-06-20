d2d2c302360f1338811426c286c8ced1c004a05e
thomas lahaye <tholahaye@laposte.net>
2023-06-20 17:36:52 +0200

    Question 3 exercice 2
    Modif de la fonction ask_coord en ask_instr pour saisir une commande avec
    prise en compte des erreurs de saisies.

--- programme_base.py
+++ programme_base.py
@@ -2,16 +2,35 @@ import sys
 
 print(sys.argv)
 
+def ask_instr():
+    stop = False
+    result = None
+    while not stop:
+        instr = input("> ")
+        split_instr = instr.split(" ")
+        try:
+            if len(split_instr) == 2:
+                x = int(split_instr[0])
+                y = int(split_instr[1])
+                result = f"Ouvrir la case {x}, {y}"
+                stop = True
+            elif len(split_instr) == 3 and split_instr[0] == "F":
+                x = int(split_instr[1])
+                y = int(split_instr[2])
+                result = f"Flagger la case {x}, {y}"
+                stop = True
+            else:
+                print("Commande invalide")
+        except ValueError:
+            print("Coordonées invalides")
 
-def ask_coord():
-    x = input("Quelle colonne voulez vous ouvrir ? : ")
-    y = input("Quelle ligne voulez vous ouvrir ? : ")
-    return x, y
+    print(result)
+    return result
 
 
 def main():
     win = False
     loss = False
     while not win and not loss:
-        ask_coord()
+        ask_instr()
 

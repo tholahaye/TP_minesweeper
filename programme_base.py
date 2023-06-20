e8f075711639d1c42c1ec38699712d63cd59cde1
thomas lahaye <tholahaye@laposte.net>
2023-06-20 15:33:31 +0200

    Question 2
    Ajout de la fonction ask_coord qui demande à l'utilisateur
    les coordonnées de son coup et la fonction main qui appelle ask_coord
    qui appelle ask_coord tant que l'utilisateur n'a pas gagné ou perdu.

--- programme_base.py
+++ programme_base.py
@@ -1,5 +1,17 @@
 import sys
 
+print(sys.argv)
 
 
+def ask_coord():
+    x = input("Quelle colonne voulez vous ouvrir ? : ")
+    y = input("Quelle ligne voulez vous ouvrir ? : ")
+    return x, y
+
+
+def main():
+    win = False
+    loss = False
+    while not win and not loss:
+        ask_coord()
 

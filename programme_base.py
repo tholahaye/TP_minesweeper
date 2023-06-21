e32537144651e3a1ed25c01c2da3ec20f950f38c
thomas lahaye <tholahaye@laposte.net>
2023-06-21 16:22:49 +0200

    Question 6 exercice 2
    Mise attribut de nb_colonnes et nb_lignes dans la classe MineSweeper

--- programme_base.py
+++ programme_base.py
@@ -6,9 +6,23 @@ class MineSweeper:
         self.win = False
         self.loss = False
         self.is_playing = False
+        self.nb_colonnes = int(sys.argv[1])
+        self.nb_lignes = int(sys.argv[2])
 
-    def new_game(self, taille_grille):
+
+    def new_game(self, nb_colonnes = None, nb_lignes = None):
         self.is_playing = True
+        if nb_colonnes is not None:
+            self.nb_colonnes = nb_colonnes
+        if nb_lignes is not None:
+            self.nb_lignes = nb_lignes
+
+
+        print(f"La grille est de {self.nb_colonnes} colonnes et {self.nb_lignes} lignes.")
+
+
+    def quit(self):
+        sys.exit("Fin de jeu")
 
     def open(self, x, y):
         if not self.is_playing:
@@ -23,15 +37,23 @@ class MineSweeper:
             print(f"Flagger la case {x}, {y}")
 
 
-def ask_instr():
+def ask_instr(minesweeper):
     stop = False
     result = None
     while not stop:
         instr = input("> ")
         split_instr = instr.split(" ")
-        minesweeper = MineSweeper()
         try:
-            if len(split_instr) == 2:
+            if split_instr[0].upper() == "QUIT":
+                minesweeper.quit()
+            elif split_instr[0].upper() == "NEWGAME":
+                if len(split_instr) == 3:
+                    minesweeper.new_game(int(split_instr[1]), int(split_instr[2]))
+                elif len(split_instr) == 1:
+                    minesweeper.new_game()
+                else :
+                    print("Commande invalide")
+            elif len(split_instr) == 2:
                 x = int(split_instr[0])
                 y = int(split_instr[1])
                 minesweeper.open(x, y)
@@ -51,10 +73,7 @@ def ask_instr():
 def main():
     win = False
     loss = False
-    nb_colonnes = sys.argv[1]
-    nb_lignes = sys.argv[2]
-
-    taille_grille = (nb_colonnes, nb_lignes)
+    minesweeper = MineSweeper()
 
     while (not win) and (not loss):
-        ask_instr()
+        ask_instr(minesweeper)

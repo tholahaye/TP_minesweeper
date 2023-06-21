45c7a5d207c355b64d08a7257b575b11209785a6
thomas lahaye <tholahaye@laposte.net>
2023-06-21 17:21:03 +0200

    Question 7 exercice 2
    Gestion de l'exception "NonegameError" et suppression de la méthode quit().

--- programme_base.py
+++ programme_base.py
@@ -1,5 +1,7 @@
 import sys
 
+class NoneGameError(Exception):
+    pass
 
 class MineSweeper:
     def __init__(self):
@@ -9,71 +11,65 @@ class MineSweeper:
         self.nb_colonnes = int(sys.argv[1])
         self.nb_lignes = int(sys.argv[2])
 
-
-    def new_game(self, nb_colonnes = None, nb_lignes = None):
+    def new_game(self, nb_colonnes=None, nb_lignes=None):
         self.is_playing = True
         if nb_colonnes is not None:
             self.nb_colonnes = nb_colonnes
         if nb_lignes is not None:
             self.nb_lignes = nb_lignes
-
-
         print(f"La grille est de {self.nb_colonnes} colonnes et {self.nb_lignes} lignes.")
-
-
-    def quit(self):
-        sys.exit("Fin de jeu")
 
     def open(self, x, y):
         if not self.is_playing:
-            raise Exception("La partie n'est pas en cours.")
+            raise NoneGameError
         else:
             print(f"Ouvrir la case {x}, {y}")
 
     def flag(self, x, y):
         if not self.is_playing:
-            raise Exception("La partie n'est pas en cours.")
+            raise NoneGameError
         else:
             print(f"Flagger la case {x}, {y}")
 
 
 def ask_instr(minesweeper):
-    stop = False
-    result = None
-    while not stop:
+
+    while True:
         instr = input("> ")
         split_instr = instr.split(" ")
         try:
             if split_instr[0].upper() == "QUIT":
-                minesweeper.quit()
+                return True
             elif split_instr[0].upper() == "NEWGAME":
                 if len(split_instr) == 3:
                     minesweeper.new_game(int(split_instr[1]), int(split_instr[2]))
                 elif len(split_instr) == 1:
                     minesweeper.new_game()
-                else :
+                else:
                     print("Commande invalide")
             elif len(split_instr) == 2:
                 x = int(split_instr[0])
                 y = int(split_instr[1])
                 minesweeper.open(x, y)
-                stop = True
+                return False
             elif len(split_instr) == 3 and split_instr[0].upper() == "F":
                 x = int(split_instr[1])
                 y = int(split_instr[2])
                 minesweeper.flag(x, y)
-                stop = True
+                return False
             else:
                 print("Commande invalide")
         except ValueError:
             print("Coordonées invalides")
-    return result
+        except NoneGameError:
+            print("La partie n'est pas en cours. Veuillez démarrer une partie.")
 
 
 def main():
     win = False
     loss = False
+    should_quit = False
     minesweeper = MineSweeper()
 
-    while (not win) and (not loss):
-        ask_instr(minesweeper)
+    while (not win) and (not loss) and not should_quit:
+        should_quit = ask_instr(minesweeper)
